f6e4ddc9b34ad906131eb07f8557dc1700f0e257
Marcel <marcelinko96@gmail.com>
2022-04-20 20:11:49 +0200

    Initial commit

---
+++ src/main/java/pl/locon/demo/LoconBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.locon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoconBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoconBackendApplication.class, args);
+	}
+
+}
